03a20fb4a653b87cbb5adb9262b0a1dbdfd6e18b
DavidLewensohn <davidlewensohn@gmail.com>
2022-06-16 14:27:01 +0300

    adding js

--- js/memeControler.js
+++ js/memeControler.js
@@ -4,27 +4,25 @@ renderMeme()
 
 
 function renderMeme() {
-    let gElCanvas = document.querySelector('canvas')
-    let gCtx = gElCanvas.getContext('2d')
-    console.log(gCtx);
+    let elCanvas = document.querySelector('canvas')
+    let ctx = elCanvas.getContext('2d')
 
     var img = new Image()
-    img.src = 'meme-imgs-s/2.jpg'
+    var meme = getMeme()
+    img.src = meme.imgSrc
 
 
     //     renering image:
     img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-        console.log('img.onload:', img.onload);
+        ctx.drawImage(img, 0, 0, elCanvas.width, elCanvas.height)
 
-        //     rendering text:
+        //     rendering text: '90px Arial'
 
-        var meme = getMeme()
         var y = 100, x = 100, text = meme.text
-        gCtx.font = meme.font
-        gCtx.fillStyle = meme.color
-        gCtx.fillText(text, x, y)
-        gCtx.strokeText(text, x, y)
+        ctx.font =  `${meme.fontSize}px ${meme.fontFamily}`
+        ctx.fillStyle = meme.color.value
+        ctx.fillText(text, x, y)
+        ctx.strokeText(text, x, y)
 
     }
 
@@ -32,7 +30,7 @@ function renderMeme() {
 
 }
 
-function onInput(ev) {
+function onTextInput(ev) {
     ev.preventDefault()
     let elTextInput = document.querySelector('[name=text-input]')
     setLineTxt(elTextInput.value)
@@ -40,5 +38,21 @@ function onInput(ev) {
     renderMeme()
 }
 
+function onColorInput(ev) {
+    ev.preventDefault()
+    let elColorInput = document.querySelector('[name=color-input]')
+    setLineColor(elColorInput)
+    console.log(elColorInput)
+    renderMeme()
+}
+
+function onLargeText() {
+    setLineSizelarger()
+    renderMeme()
+}
+function onSmallText() {
+    setLineSizeSmaller()
+    renderMeme()
+}
 
 
